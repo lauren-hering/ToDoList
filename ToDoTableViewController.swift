272e8eb6039f7bbbcbee33d6fab2a24554f956a0
lauren-hering <lauren.hering@comcast.net>
2019-07-15 13:30:03 -0500

    complete iteration 1

--- ToDoTableViewController.swift
+++ ToDoTableViewController.swift
@@ -31,12 +31,12 @@ class ToDoTableViewController: UITableViewController {
         toDos = createToDos()
     }
 
+    
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+   
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -94,14 +94,31 @@ class ToDoTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+       
+        if let addVC = segue.destination as? AddToDOViewController {
+            addVC.previousVC = self
+        }
+        if let completeVC = segue.destination as? CompleteToDoViewController {
+            if let toDo = sender as? ToDo {
+                completeVC.selectedToDo = toDo
+                completeVC.previousVC = self
+            }
+        }
+        
     }
-    */
+    
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+       
+        let toDo = toDos[indexPath.row]
+        
+        performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
+    
 }
